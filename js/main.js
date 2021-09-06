ee0da7b61f53c33d1f2cd246d29af4548468e8b9
Dan Miller <dan@cypherstack.com>
2021-09-06 14:45:01 -0700

    Initial Epic website from Diego

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var normal = document.getElementById("nav-menu");
+var reverse = document.getElementById("nav-menu-left");
+
+var icon = normal !== null ? normal : reverse;
+
+// Toggle the "menu-open" % "menu-opn-left" classes
+function toggle() {
+	  var navRight = document.getElementById("nav-menu");
+	  var navLeft = document.getElementById("nav-left");
+      var navTwo = document.getElementById("nav2");
+	  var nav = navRight !== null ? navRight : navLeft;
+
+	  var button = document.getElementById("menu");
+	  var site = document.getElementById("wrap");
+	  
+	  if (nav.className == "menu-open" || nav.className == "menu-open-left") {
+	  	  nav.className = "";
+	  	  button.className = "";
+	  	  site.className = "";
+          navTwo.className = "";
+	  } else if (reverse !== null) {
+	  	  nav.className += "menu-open-left";
+	  	  button.className += "btn-close";
+	  	  site.className += "fixed";
+	  } else {
+	  	  nav.className += "menu-open";
+	  	  button.className += "btn-close";
+	  	  site.className += "fixed";
+          navTwo.className += "menu-open";
+	    }
+	}
+
+// Ensures backward compatibility with IE old versions
+function menuClick() {
+	if (document.addEventListener && icon !== null) {
+		icon.addEventListener('click', toggle);
+	} else if (document.attachEvent && icon !== null) {
+		icon.attachEvent('onclick', toggle);
+	} else {
+		return;
+	}
+}
+
+menuClick();
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
